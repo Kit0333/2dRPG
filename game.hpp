ff5f72724625b7afad328837fa2bd571e088562c
Liu Florentin <100954066+Kit0333@users.noreply.github.com>
2022-11-18 14:50:17 +0100

    Create game.hpp

---
+++ game.hpp
@@ -0,0 +1,79 @@
+#ifndef __GAME_HPP__
+#define __GAME_HPP__
+#include <gameState.hpp>
+#include <sdl2/SDL.h>
+#include <sdl2/SDL_image.h>
+class Game{
+
+
+
+
+
+
+
+
+
+
+
+
+
+public:
+
+    Game(const char* title,Uint32 flags,int &width,int &height);
+
+    ~Game();
+
+
+
+    SDL_Texture* loadTexture(const char* filePath);
+
+    void render(SDL_Texture* tex,int x=0,int y=0,int w=1024,int h=600);
+
+    GameState getGameState();
+
+    int& mousePress(SDL_MouseButtonEvent& b);
+
+    void gameStateChange(GameState x);
+
+    
+
+    Player getActualPlayer();
+
+    void playerSwitch();
+
+    int selectNewPawn(int x,int y,int screenWidth,int screenHeight);
+
+
+
+    void run();
+
+    void clear();
+
+    void display();
+
+    void cleanup();
+
+private:
+
+
+
+
+
+    void UserScreenSize(int&width, int &height);
+
+    Player m_player;
+
+    GameState m_gameState;
+
+    SDL_Window* m_window;
+
+    SDL_Renderer* m_renderer;
+
+};
+ 
+  
+
+  
+
+
+#endif
