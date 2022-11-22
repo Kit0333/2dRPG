9f72726a12b94abcca36651cf2d0014894654ae0
Liu Florentin <100954066+Kit0333@users.noreply.github.com>
2022-11-22 11:24:01 +0100

    Update map.hpp

--- map.hpp
+++ map.hpp
@@ -1,14 +1,16 @@
 #ifndef __MAP_HPP__
   #define __MAP_HPP__
-class Map{
-public:
-  void changeMap(int mapNum);
-private:
-  int _dimensionX;
-  int _dimensionY;
-  int *currentMap;
-  int *listMap;
+  
+  class Map{
+  public:
+     void changeMap(int *mapNum);
+     int currentMapNum();
+  private:
+     int _dimensionX;
+     int _dimensionY;
+     int *_currentMap;
+     int *_listMap;
 
-};
+   };
 
 #endif 
