e240501f607a1bc7a8c94ef9de661beb01496595
Liu Florentin <100954066+Kit0333@users.noreply.github.com>
2022-11-22 11:13:35 +0100

    Update gameState.hpp

--- gameState.hpp
+++ gameState.hpp
@@ -1,5 +1,8 @@
-#include <iostream>
+#ifndef __GAMESTATE_HPP__
+#define __GAMESTATE_HPP__
 
 enum class GameState{PLAY,EXIT};
 
 // There is two state of the game, PLAY when the game is still processering and EXIT when the game is about to close.
+
+#endif
